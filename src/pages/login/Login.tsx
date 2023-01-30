6e28675540807994d7b9d03291e467531c56b0f8
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-01-30 12:20:50 +0330

    feat: :lipstick: add login page with style

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,30 @@
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            quae, odio voluptatem neque voluptatibus veritatis perferendis
+            praesentium tenetur.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
