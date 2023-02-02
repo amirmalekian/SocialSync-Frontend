8969d73b1fb7089d084f5acbac659efd3e27a3ad
Amirhossein Malekian <amirho3einmalekian@gmail.com>
2023-02-02 23:35:23 +0330

    feat: :sparkles: add stories section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
-import './home.scss'
+import Stories from "../../components/stories/Stories";
+import "./home.scss";
 
 const Home = () => {
-  return <div className='home'>Home</div>;
+  return (
+    <div className="home">
+      <Stories />
+    </div>
+  );
 };
 
 export default Home;
